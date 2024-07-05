575f0f2f8821812bc5ba0b715e652100e2215329
NikshayMohla <78416149+NikshayMohla@users.noreply.github.com>
2024-07-05 14:22:28 +0530

    functions

--- script.js
+++ script.js
@@ -22,5 +22,74 @@ handleSlider();
 function handleSlider(){
     inputSlider.value=passwordLength;
     lengthDisplay.innerText=passwordLength;
+}
 
+function setIndicator(color)
+{
+    indicator.computedStyleMap.backgroundColor=color;
+
+}
+
+function getRandomInteger(min,max){
+    return Math.floor(random()*(max-min))+min;
+}
+
+function generateRandomNumber(){
+    return getRandomInteger(0,9);
+}
+
+function generateLowercase(){
+    return String.fromCharCode(getRandomInteger(97,123));
+}
+
+function generateUppercase(){
+    return String.fromCharCode(getRandomInteger(65,91));
+}
+
+function generateSymbol(){
+    const randNum = getRandomInteger(0,symbols.length());
+    return symbols.charAt(randNum);
+}
+
+function calcStrength()
+{
+    const randNum = getRndInteger(0, symbols.length);
+    return symbols.charAt(randNum);
+}
+
+function calcStrength() {
+    let hasUpper = false;
+    let hasLower = false;
+    let hasNum = false;
+    let hasSym = false;
+    if (uppercaseCheck.checked) hasUpper = true;
+    if (lowercaseCheck.checked) hasLower = true;
+    if (numbersCheck.checked) hasNum = true;
+    if (symbolsCheck.checked) hasSym = true;
+  
+    if (hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8) {
+      setIndicator("#0f0");
+    } else if (
+      (hasLower || hasUpper) &&
+      (hasNum || hasSym) &&
+      passwordLength >= 6
+    ) {
+      setIndicator("#ff0");
+    } else {
+      setIndicator("#f00");
+    }
+}
+
+async function copyContent(){
+    try {
+        await navigator.clipboard.writeText(passwordDisplay.value);
+        copyMsg.innerText="copied";
+    }
+    catch(e){
+        copyMsg.innerText="Failed";
+    }
+    copyMsg.classList.add("active");
+    setTimeout(()=>{
+        copyMsg.classlist.remove("active");
+    },2000);
 }
